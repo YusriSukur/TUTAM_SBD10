855184bdddb25dbc38352ba6381dd10c435834e8
YusriSukur <Yusrisukur@gmail.com>
2026-05-03 16:52:40 +0700

    install pg

--- frontend/api/login.js
+++ frontend/api/login.js
@@ -1,14 +1,22 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
 import bcrypt from 'bcryptjs';
+
+const { Pool } = pkg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
 
 export default async function handler(req, res) {
-  const { email, password } = req.body;
+  if (req.method !== 'POST') {
+    return res.status(405).json({ message: 'Method not allowed' });
+  }
 
   try {
+    console.log("ENV:", process.env.DATABASE_URL);
+
+    const { email, password } = req.body;
+
     const result = await pool.query(
       'SELECT * FROM users WHERE email = $1',
       [email]
@@ -26,10 +34,14 @@ export default async function handler(req, res) {
       return res.status(400).json({ message: 'Wrong password' });
     }
 
-    res.status(200).json({ message: 'Login success', user });
+    res.status(200).json({ message: 'Login success' });
 
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: 'Login error' });
+    console.error("ERROR LOGIN:", err);
+    res.status(500).json({ message: err.message });
   }
 }
+
+export const config = {
+  runtime: 'nodejs'
+};
